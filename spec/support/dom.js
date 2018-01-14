a5be166083c3c229f1189d8d1864d6159b4cbe36
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2018-01-14 10:37:02 +0100

    Feat: add tests for generateUser function

---
+++ spec/support/dom.js
@@ -0,0 +1,34 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const addElements = () => {
+  const event = { target: [{ value: "pierroberto" }] };
+  return JSDOM.fromFile("./index.html", {}).then(dom => {
+    return fetchUserData(event).then(userList => {
+      return getPersonalInfo("https://api.github.com/users/pierroberto").then(
+        userData => {
+          const document = dom.window.document;
+          const avatar = document.createElement("img");
+          const login = document.createTextNode("@pierroberto");
+          const username = document.createTextNode("pierroberto");
+          const fullName = document.createTextNode("pier roberto");
+          const biography = document.createTextNode("very long bio");
+          avatar.src = "https://avatars1.githubusercontent.com/u/28726124?v=4";
+          document
+            .getElementsByClassName("user__avatar")[0]
+            .appendChild(avatar);
+          document.getElementsByClassName("user__login")[0].appendChild(login);
+          document
+            .getElementsByClassName("user__bio")[0]
+            .appendChild(biography);
+          document
+            .getElementsByClassName("user__fullName")[0]
+            .appendChild(fullName);
+          return document;
+        }
+      );
+    });
+  });
+};
+
+module.exports = { addElements };
--- spec/support/spec.js
+++ spec/support/spec.js
@@ -1,81 +1,35 @@
-const rewire = require("rewire");
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
 require("es6-promise").polyfill();
 require("isomorphic-fetch");
-
+const rewire = require("rewire");
 const app = rewire("../../js/index.js");
+const dom = require("./dom.js");
+
 // I get the functions which I need to test
-test = app.__get__("test");
 retrieveData = app.__get__("retrieveData");
 getPersonalInfo = app.__get__("getPersonalInfo");
 fetchUserData = app.__get__("fetchUserData");
 
-// describe("Test search bar", function() {
-//   it("should return true for each user found", function() {
-//     const { document } = new JSDOM(
-//       `<body>
-//
-//       <div class="wrapper">
-//         <div class="container">
-//           <div class="search">
-//             <form class="search__form" onSubmit="retrieveData(event)">
-//             <input class="search__input" ref="user" name="user" placeholder="Search username..."/>
-//             <button type="submit" class="search__button">Search</button>
-//           </form>
-//           </div>
-//           <div class="user">
-//             <div class="user__noresult"></div>
-//             <div class="user__container">
-//               <div class="user__avatar"></div>
-//               <div class="user__info">
-//                 <div class="user__login"></div>
-//                 <div class="user__fullName"></div>
-//                 <div class="user__bio"></div>
-//               </div>
-//             </div>
-//           </div>
-//           <div class="repos">
-//             <div class="repos__header"></div>
-//             <div class="repos__list">
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//       </body>`
-//     ).window;
-//     // return JSDOM.fromFile("./index.html", {}).then(dom => {
-//     //   const html = dom.window;
-//     //
-//     //   return dom;
-//     // });
-//     console.log("doc", document);
-//     const event = { target: [{ value: "pierroberto" }] };
-//     expect(retrieveData(event)).toBe(true);
-//   });
-// });
-
-// describe("Retrieve personal information", () => {
-//   const validUrl = "https://api.github.com/users/pierroberto";
-//   const invalidUrl = "https://api.github.com/";
-//   it("Should return an object", () => {
-//     return getPersonalInfo(validUrl).then(response => {
-//       expect(typeof response).toEqual("object");
-//     });
-//   });
-//   it("should have a biography and name", () => {
-//     return getPersonalInfo(validUrl).then(response => {
-//       expect(typeof response.bio).toEqual("string");
-//       expect(typeof response.name).toEqual("string");
-//     });
-//   });
-//   it("should return bio and name undefined if url is not valid", () => {
-//     return getPersonalInfo(invalidUrl).then(response => {
-//       expect(response.bio).toBe(undefined);
-//       expect(response.name).toBe(undefined);
-//     });
-//   });
-// });
+describe("Retrieve personal information", () => {
+  const validUrl = "https://api.github.com/users/pierroberto";
+  const invalidUrl = "https://api.github.com/";
+  it("Should return an object", () => {
+    return getPersonalInfo(validUrl).then(response => {
+      expect(typeof response).toEqual("object");
+    });
+  });
+  it("should have a biography and name", () => {
+    return getPersonalInfo(validUrl).then(response => {
+      expect(typeof response.bio).toEqual("string");
+      expect(typeof response.name).toEqual("string");
+    });
+  });
+  it("should return bio and name undefined if url is not valid", () => {
+    return getPersonalInfo(invalidUrl).then(response => {
+      expect(response.bio).toBe(undefined);
+      expect(response.name).toBe(undefined);
+    });
+  });
+});
 
 describe("Retrieve the correct information for the user", () => {
   const event = { target: [{ value: "pierroberto" }] };
@@ -92,5 +46,16 @@ describe("Retrieve the correct information for the user", () => {
   it("event object has to have a target.value", () => {
     // I check the first letter of the event.target.value
     expect(event.target[0].value[0]).not.toBe(undefined);
+  });
+});
+
+describe("Add elements to the DOM", () => {
+  it("Should add nodes from the parent", async () => {
+    const document = await dom.addElements();
+    const avatar = document.createElement("img");
+    expect(document.querySelector(".user__avatar")).not.toBe(null);
+    expect(document.querySelector(".user__login")).not.toBe(null);
+    expect(document.querySelector(".user__bio")).not.toBe(null);
+    expect(document.querySelector(".user__fullName")).not.toBe(null);
   });
 });
