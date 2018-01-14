c9521ca675154b90361d61cc6d8e14bd4759feff
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2018-01-14 12:10:19 +0100

    Chore: uncomment some working tests

--- spec/support/spec.js
+++ spec/support/spec.js
@@ -10,45 +10,45 @@ getPersonalInfo = app.__get__("getPersonalInfo");
 fetchUserData = app.__get__("fetchUserData");
 getRepos = app.__get__("getRepos");
 
-// describe("Retrieve personal information", () => {
-//   const validUrl = "https://api.github.com/users/pierroberto";
-//   const invalidUrl = "https://api.github.com/";
-//   it("Should return an object", () => {
-//     return getPersonalInfo(validUrl).then(response => {
-//       expect(typeof response).toEqual("object");
-//     });
-//   });
-//   it("should have a biography and name", () => {
-//     return getPersonalInfo(validUrl).then(response => {
-//       expect(typeof response.bio).toEqual("string");
-//       expect(typeof response.name).toEqual("string");
-//     });
-//   });
-//   it("should return bio and name undefined if url is not valid", () => {
-//     return getPersonalInfo(invalidUrl).then(response => {
-//       expect(response.bio).toBe(undefined);
-//       expect(response.name).toBe(undefined);
-//     });
-//   });
-// });
-//
-// describe("Retrieve the correct information for the user", () => {
-//   const event = { target: [{ value: "pierroberto" }] };
-//   it("should return a response", () => {
-//     return fetchUserData(event).then(response => {
-//       expect(typeof response.total_count).toEqual("number");
-//     });
-//   });
-//   it("should return one user", () => {
-//     return fetchUserData(event).then(response => {
-//       expect(response.total_count).toEqual(1);
-//     });
-//   });
-//   it("event object has to have a target.value", () => {
-//     // I check the first letter of the event.target.value
-//     expect(event.target[0].value[0]).not.toBe(undefined);
-//   });
-// });
+describe("Retrieve personal information", () => {
+  const validUrl = "https://api.github.com/users/pierroberto";
+  const invalidUrl = "https://api.github.com/";
+  it("Should return an object", () => {
+    return getPersonalInfo(validUrl).then(response => {
+      expect(typeof response).toEqual("object");
+    });
+  });
+  it("should have a biography and name", () => {
+    return getPersonalInfo(validUrl).then(response => {
+      expect(typeof response.bio).toEqual("string");
+      expect(typeof response.name).toEqual("string");
+    });
+  });
+  it("should return bio and name undefined if url is not valid", () => {
+    return getPersonalInfo(invalidUrl).then(response => {
+      expect(response.bio).toBe(undefined);
+      expect(response.name).toBe(undefined);
+    });
+  });
+});
+
+describe("Retrieve the correct information for the user", () => {
+  const event = { target: [{ value: "pierroberto" }] };
+  it("should return a response", () => {
+    return fetchUserData(event).then(response => {
+      expect(typeof response.total_count).toEqual("number");
+    });
+  });
+  it("should return one user", () => {
+    return fetchUserData(event).then(response => {
+      expect(response.total_count).toEqual(1);
+    });
+  });
+  it("event object has to have a target.value", () => {
+    // I check the first letter of the event.target.value
+    expect(event.target[0].value[0]).not.toBe(undefined);
+  });
+});
 
 describe("Retrieve the repositories of the user", () => {
   const validLogin = "pierroberto";
@@ -65,25 +65,25 @@ describe("Retrieve the repositories of the user", () => {
   });
 });
 
-// describe("Add elements to the DOM", () => {
-//   it("Should add nodes to the parent", async () => {
-//     const document = await dom.addElements();
-//     expect(document.querySelector(".user__avatar")).not.toBe(null);
-//     expect(document.querySelector(".user__login")).not.toBe(null);
-//     expect(document.querySelector(".user__bio")).not.toBe(null);
-//     expect(document.querySelector(".user__fullName")).not.toBe(null);
-//   });
-//   it("Should delete node from the parent", async () => {
-//     const toBeDeleted = [
-//       "user__login",
-//       "user__fullName",
-//       "user__bio",
-//       "user__avatar"
-//     ];
-//     const document = await dom.deleteElements(toBeDeleted);
-//     expect(document.querySelector(".user__avatar").firstChild).toBe(null);
-//     expect(document.querySelector(".user__login").firstChild).toBe(null);
-//     expect(document.querySelector(".user__bio").firstChild).toBe(null);
-//     expect(document.querySelector(".user__fullName").firstChild).toBe(null);
-//   });
-// });
+describe("Add elements to the DOM", () => {
+  it("Should add nodes to the parent", async () => {
+    const document = await dom.addElements();
+    expect(document.querySelector(".user__avatar")).not.toBe(null);
+    expect(document.querySelector(".user__login")).not.toBe(null);
+    expect(document.querySelector(".user__bio")).not.toBe(null);
+    expect(document.querySelector(".user__fullName")).not.toBe(null);
+  });
+  it("Should delete node from the parent", async () => {
+    const toBeDeleted = [
+      "user__login",
+      "user__fullName",
+      "user__bio",
+      "user__avatar"
+    ];
+    const document = await dom.deleteElements(toBeDeleted);
+    expect(document.querySelector(".user__avatar").firstChild).toBe(null);
+    expect(document.querySelector(".user__login").firstChild).toBe(null);
+    expect(document.querySelector(".user__bio").firstChild).toBe(null);
+    expect(document.querySelector(".user__fullName").firstChild).toBe(null);
+  });
+});
